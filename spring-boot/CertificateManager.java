d18c6b3e086b105bbe0a84b53075f846dfe7a797
Steven PJ <spj-consulting@proton.me>
2025-08-08 20:09:35 -0700

    Remove unused import

--- spring-boot/CertificateManager.java
+++ spring-boot/CertificateManager.java
@@ -11,7 +11,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class CertificateManager {
